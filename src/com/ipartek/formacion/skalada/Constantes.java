b8bb49cda0d17b030575f8411c1e8415135b4ed1
Ander Ipartek <auraga@ipartek.com>
2015-07-24 10:17:19 +0200

    initial import

---
+++ src/com/ipartek/formacion/skalada/Constantes.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion.skalada;
+
+public class Constantes {
+
+	public static final String ROOT_APP = "/skalada/";
+	
+	//vistas publicas
+	public static final String VIEW_PUBLIC_INDEX = ROOT_APP + "index.jsp";
+	
+	//vistas backoffice
+	public static final String ROOT_BACK = ROOT_APP + "backoffice/";
+	public static final String VIEW_BACK_LOGIN = ROOT_BACK + "pages/login.jsp";
+	public static final String VIEW_BACK_INDEX = ROOT_BACK + "pages/index_back.jsp";
+	
+	
+	//controladores
+	public static final String CONTROLLER_LOGIN = ROOT_APP + "login";
+	
+	
+}
