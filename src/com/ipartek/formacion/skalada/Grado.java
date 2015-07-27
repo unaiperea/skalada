f80c5282bae31aedf1440006322a72cd8dfb24e7
cursoip2015 <cursoipartek2015@gmail.com>
2015-07-27 12:43:56 +0200

    sss

--- src/com/ipartek/formacion/skalada/Grado.java
+++ src/com/ipartek/formacion/skalada/Grado.java
@@ -1,13 +1,14 @@
 package com.ipartek.formacion.skalada;
 
+import java.io.Serializable;
+
 /**
-<<<<<<< HEAD
  * 
  * Grado de dificultad para las vías o sectores
  * @author Curso
  *
  */
-public enum Grado {
+public enum Grado implements Serializable{
 
 	//TODO luego será una tabla auxiliar en la bbdd
 	FACIL, NORMAL, DIFICIL, EXTREMO; 
