fe08ad244f54c24e167edd8458038de177590014
javi70 <javi70@gmail.com>
2015-09-21 12:15:39 +0200

    sentencias sql usuario

---
+++ script/sentencias_usuarios.sql
@@ -0,0 +1,21 @@
+-- GETONE
+SELECT u.`id`, u.`email`, u.`nombre`, u.`password`, u.`validado`, u.`id_rol`, r.`nombre`
+FROM `usuario` AS u
+INNER JOIN `rol` as r
+ON (u.`id_rol` = r.`id`) 
+WHERE u.`id`= ?;
+
+-- GETALL
+SELECT u.`id`, u.`email`, u.`nombre`, u.`password`, u.`validado`, u.`id_rol`, r.`nombre`
+FROM `usuario` AS u
+INNER JOIN `rol` as r
+ON (u.`id_rol` = r.`id`); 
+
+-- DELETE
+DELETE FROM `usuario` WHERE `id`=?;
+
+-- UPDATE
+UPDATE `usuario` SET `email`=?, `nombre`=?, `password`=?, `validado`=? WHERE `id`= ?;
+
+-- INSERT
+INSERT INTO `usuario` (`email`, `nombre`, `password`, `id_rol`) VALUES (?, ?, ?, ?);
