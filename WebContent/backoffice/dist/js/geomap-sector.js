1a0752eb44f019941190ce7f0a26e65c38da49bc
Curso <Curso@GBRB5.ipartekaula.com>
2015-10-21 12:18:58 +0200

    Editado visualizacion de Zonas (circulo)

--- WebContent/backoffice/dist/js/geomap-sector.js
+++ WebContent/backoffice/dist/js/geomap-sector.js
@@ -1,20 +1,22 @@
 var map;
 var latSector = document.getElementsByName('latitud')[0].value;
 var lngSector = document.getElementsByName('longitud')[0].value;
-var zoom = 15;
+var zoom = 14;
 
 function show_map(localizacion) {
 	
-	//obtener localizacion
-	
-	var latZona = localizacion.coords.latitude;//document.getElementsByName('zona')[0].dataset.lat;
-	var lngZona = localizacion.coords.longitude;//document.getElementsByName('zona')[0].selectedOption.dataset.lng;
+	//obtener localizacion	
+	var selectZonas = document.getElementById("selecZonas");
+    var selectedOption = selectZonas.options[selectZonas.selectedIndex];	
+	var latZona = selectedOption.dataset.lat;
+	var lngZona = selectedOption.dataset.lng;
 	
 	if(latSector == 0.0 && lngSector == 0.0){
 		latSector = latZona;
 		lngSector = lngZona;
 	}	
 	var center = new google.maps.LatLng(latZona,lngZona);
+	var sectorPosition = new google.maps.LatLng(latSector,lngSector);
 	
 	//inicializar Mapa
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -26,7 +28,7 @@ function show_map(localizacion) {
 	
 	//marcador
 	var marker = new google.maps.Marker({
-		position: center,
+		position: sectorPosition,
 		map: map,
 		draggable: true,
 	 	title: document.getElementsByName('nombre')[0].value
@@ -46,14 +48,13 @@ function show_map(localizacion) {
 function changeMapPosition(){
 	
 	var selectZonas = document.getElementById("selecZonas");
-    var selectedOption = selectZonas.options[selectZonas.selectedIndex];
-	
+    var selectedOption = selectZonas.options[selectZonas.selectedIndex];	
 	var lat = selectedOption.dataset.lat;
 	var lng = selectedOption.dataset.lng;
 	
 	console.debug(" Moviendo mapa {" + lat + "," + lng +"}" );
 	var newLatLng = new google.maps.LatLng( lat, lng);
-	map.setZoom(15);
+	map.setZoom(zoom);
 	map.setCenter(newLatLng);
 }
 
