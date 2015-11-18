44b98acfeb3f1a0959deb00d9e425e9a963243d3
aker <aker@Urtzi>
2015-11-18 20:39:13 +0100

    Preparándolo para openshift

--- src/main/java/com/ipartek/formacion/skalada/Constantes.java
+++ src/main/java/com/ipartek/formacion/skalada/Constantes.java
@@ -18,7 +18,7 @@ public final class Constantes {
 		super();
 	}
 
-	public static final String SERVER = "http://localhost:8080";
+	public static final String SERVER = "http://project-eskalada.rhcloud.com:8080";
 	public static final String ROOT_APP = "/skalada/";
 	public static final String APP_NAME = "skalada";
 
